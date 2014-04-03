c0b21660016b8e26f6937b31ec339795a265455d
Chris Wharton <chrisw@catalyst.net.nz>
2014-04-04 11:29:20 +1300

    fix require for config.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require('/home/chrisw/dev/topnz-moodle/htdocs/config.php');
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once($CFG->dirroot.'/course/lib.php');
 require_once($CFG->dirroot.'/report/linkvalidator/lib.php');
 require_once($CFG->dirroot.'/report/linkvalidator/locallib.php');
